c548ac64255965c7e40c259b8b1d6c652bd80d04
loreto bustos <loretobustoscampos@gmail.com>
2020-10-09 14:16:56 -0300

    mi primer github

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+ <h1>hola</h1>
+  <ListaDeTareas> </ListaDeTareas>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ListaDeTareas from './components/ListaDeTareas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListaDeTareas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <h1>Lista de Componentes</h1>
+
+    <form>
+        <label for="">Tareas</label>
+        <input type="text" v-model="tarea">
+        <button @click.prevent="agregar_tarea">Añade</button>
+    </form>
+    <ul>
+        <li v-for="tarea in lista_tareas" :key="tarea">
+
+            {{ tarea}}
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Lista_de_tareas',
+    // props: {},
+    data: function(){
+        return {
+            mensaje:'hola mundo',
+            tarea: '',
+            lista_tareas: [],
+        }
+    },
+    // computed: {},
+    methods: {
+        // -- Metodos
+        agregar_tarea (){
+            alert(this.tarea)
+            this.lista_tareas.push(this.tarea)
+        }
+    },
+    // components: {},
+}
+</script>
+
+<style scoped>
+
+</style>
